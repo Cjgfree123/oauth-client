50e8eee49ec4f1d8d6c09ebb4698f9e18a25ffd9
jingechen <jingechen@sohu-inc.com>
2019-10-03 21:26:26 +0800

    add:授权

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -19,6 +21,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: true,
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({
+      url: "mongodb://127.0.0.1",
+    })
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
--- routes/user.js
+++ routes/user.js
@@ -24,6 +24,66 @@ router.get('/login', function (req, res, next) {
 // /user/callback
 router.get("/callback", function (req, res, next) {
   let { code } = req.query;
+  let options = {
+    redirect_uri,
+    grant_type: 'authorization_code',
+    client_id: appId,
+    client_secret: appKey,
+    code: "xxxxx",
+  };
+  let fullFetchTokenUrl = fetchTokenUrl + qs.stringify(options);
+  logger("fullFetchTokenUrl", fullFetchTokenUrl);
+
+  let res = await request(fullFetchTokenUrl);
+  logger("res", res);
+  // 1. 取得token
+  let {
+    access_token,
+    expires_in,
+    refresh_token,
+  } = qs.parse(res);
+  return res.send(FetchTokenUrlRes);
+
+  options = {
+    refresh_token,
+    grant_type: "refresh_token",
+    client_id: appId,
+    client_secret: appKey,
+  };
+
+  let FetchTokenByRefreshTokenUrl = fetchTokenUrl + qs.stringify(options);
+  let resByRefreshToken = await request(FetchTokenByRefreshTokenUrl);
+  logger("resByRefreshToken", resByRefreshToken); // 假设成功, 响应体应该是: access_token=111&expires_in=yyy&refresh_token=123(新的access_token才能用)
+
+  options = {
+    access_token
+  };
+  let fullFetchMeUrl = fetchMeUrl + qs.stringify(options);
+  logger("fullFetchMeUrl", fullFetchMeUrl);
+
+  let fetchMeRes = await request(fetchMeUrl);
+  logger("fetchMeRes", fetchMeRes); // 假设成功, 响应体应该是: 'callback( {"client_id":"YOUR_APPID","openid":"YOUR_OPENID"} );';
+
+  // 2. 通过token 取得openId
+  let {
+    openid,
+  } = JSON.parse(fetchMeRes.slice(fetchMeRes.indexOf("{"), fetchMeRes.lastIndexOf("}") + 1));
+  console.log("openid", openid);
+  return res.send(openid);
+
+  /**
+   * user info
+   */
+  options = {
+    access_token,
+    openid,
+    oauth_consumer_key: appId,
+  };
+  let fullFetchUserInfo = fetchUserInfo + qs.stringify(options);
+  let userInfoRes = await request(fullFetchUserInfo);
+  console.log("user info", userInfoRes); // 假设成功，响应体应该如下
+  let userInfo = JSON.parse(userInfoRes);
+  res.json(userInfo);
 });
 
 module.exports = router;
